dbd81bfc74835c65dc2fc034d54e3a9921cfbd54
dan <daan.forever@gmail.com>
2014-05-03 14:19:52 +0400

    Lookup in method_missing

--- app/models/concerns/settingson/base.rb
+++ app/models/concerns/settingson/base.rb
@@ -5,7 +5,11 @@ module Settingson::Base
   module ClassMethods
 
     def method_missing(string, *args)
-      ::Settingson::Store.new(self, string, *args)
+      if result = find_by(name: string)
+        YAML.load result.value
+      else
+        ::Settingson::Store.new(self, string, *args)
+      end
     end
 
   end
