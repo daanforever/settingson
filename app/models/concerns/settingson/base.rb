0fb4000fd37ce262f2ab942a3445784c427bde93
dan <daan.forever@gmail.com>
2014-05-04 21:34:26 +0400

    concern

--- app/models/concerns/settingson/base.rb
+++ app/models/concerns/settingson/base.rb
@@ -3,34 +3,30 @@ module Settingson::Base
   extend ActiveSupport::Concern
 
   included do
-    attr_accessor :settingson, :name, :value
+    attr_accessor :settingson
   end
 
   def method_missing(symbol, *args)
-    Rails.logger.debug("Instance method_missing: #{symbol} #{args.inspect} s:#{@settingson}")
+    super
+  rescue NoMethodError
+
     case symbol.to_s
     when /(.+)=/  # setter
 
-      Rails.logger.debug("\tsetter")
       @settingson = "#{@settingson}.#{$1}"
 
       if record = self.class.find_by(name: @settingson)
-        Rails.logger.debug("\tfound record for #{@settingson}")
         record.update(value: args.first.to_yaml)
       else
-        Rails.logger.debug("\trecord not found for #{@settingson}")
         self.class.create(name: @settingson, value: args.first.to_yaml)
       end
 
     else # getter
 
-      Rails.logger.debug("\tgetter")
       @settingson += ".#{symbol.to_s}"
       if record = self.class.find_by(name: @settingson)
-        Rails.logger.debug("\tfound record for #{@settingson}")
         YAML.load(record.value)
       else
-        Rails.logger.debug("\trecord not found for #{@settingson}")
         self
       end
 
@@ -40,29 +36,25 @@ module Settingson::Base
   module ClassMethods
 
     def method_missing(symbol, *args)
-      Rails.logger.debug("Class method_missing: #{symbol} #{args.inspect} #{@settingson}")
+      super
+    rescue NoMethodError
+
       case symbol.to_s
       when /(.+)=/  # setter
 
-        Rails.logger.debug("\tsetter")
         @settingson = $1
 
         if record = find_by(name: @settingson)
-          Rails.logger.debug("\tfound record for #{@settingson}")
           record.update(value: args.first.to_yaml)
         else
-          Rails.logger.debug("\trecord not found for #{@settingson}")
           create(name: @settingson, value: args.first.to_yaml, settingson: @settingson)
         end
 
       else # getter
 
-        Rails.logger.debug("\tgetter")
         if record = find_by(name: symbol.to_s)
-          Rails.logger.debug("\tfound record for #{@settingson}")
           YAML.load record.value
         else
-          Rails.logger.debug("\trecord not found for #{@settingson}")
           new(settingson: symbol.to_s)
         end
 
