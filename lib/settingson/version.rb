005259a4cef3bfadfceea1061b3471b9029777f4
dan <daan.forever@gmail.com>
2015-09-24 17:04:00 +0300

    Added a few tests

--- lib/settingson/version.rb
+++ lib/settingson/version.rb
@@ -1,3 +1,3 @@
 module Settingson
-  VERSION = "1.3.2"
+  VERSION = "1.3.3"
 end
--- spec/models/settings_spec.rb
+++ spec/models/settings_spec.rb
@@ -7,32 +7,58 @@ describe Settings do
   it 'not raises error on create new element' do
     expect{ Settings.hello = Faker::Lorem.word }.to_not raise_error
   end
-  it 'returns same value' do
+  it 'returns same Fixnum' do
+    word = Faker::Lorem.word
+    Settings.number = 100
+    expect( Settings.number ).to eq(100)
+  end
+  it 'returns same String' do
     word = Faker::Lorem.word
     Settings.hello = word
     expect( Settings.hello ).to eq(word)
   end
-  it 'returns same value for composit (1)' do
+  it 'returns same value for complex key #1' do
     word = Faker::Lorem.word
     Settings.hello.hello = word
     expect( Settings.hello.hello ).to eq(word)
   end
-  it 'returns same value for composit (2)' do
+  it 'returns same value for complex key #2' do
     word = Faker::Lorem.word
     Settings.i.hello = word
     expect( Settings.i.hello ).to eq(word)
   end
 
-  it 'destroys record with nil value (1)' do
+  it 'destroys record with nil value #1' do
     word = Faker::Lorem.word
     Settings.some = word
     expect{ Settings.some = nil }.to change{ Settings.count }.by(-1)
   end
 
-  it 'destroys record with nil value (2)' do
+  it 'destroys record with nil value #2' do
     word = Faker::Lorem.word
     Settings.some.hello = word
     expect{ Settings.some.hello = nil }.to change{ Settings.count }.by(-1)
   end
 
+  describe 'with empty value' do
+    it 'returns empty string' do
+      expect( "#{Settings.not_found}" ).to eq("")
+    end
+
+    it 'returns value.nil? is true' do
+      expect( Settings.not_found.nil? ).to be(true)
+    end
+
+    it 'returns value.empty? is true' do
+      expect( Settings.not_found.empty? ).to be(true)
+    end
+
+    it 'returns value.blank? is true' do
+      expect( Settings.not_found.blank? ).to be(true)
+    end
+
+    it 'returns value.present? is false' do
+      expect( Settings.not_found.present? ).to be(false)
+    end
+  end
 end
